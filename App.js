bbb9b7c3dec5f58458b0628c4c42f149946eeda1
Elise Yang <elisenyang@Elises-MacBook-Pro-2.local>
2017-07-14 10:37:18 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ListView,
+  Alert,
+  Button
+} from 'react-native';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <Image source={require('./images/login.jpeg')} style={styles.container}>
+      <Text>Login</Text>
+      <TextInput style={{padding: 10, height: 40}}
+          placeholder="Username"
+        />
+        <TextInput style={{padding: 10, height: 40}}
+          placeholder="Password"
+          secureTextEntry={true}
+        />
+        <TouchableOpacity>
+          <Text>Tap to Login</Text>
+        </TouchableOpacity>
+      </Image>
+    );
+  }
+}
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    width: undefined,
+    height: undefined,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
